d80c5bad318b05d98f4635283e555ac6e8f8a248
Chanin <k.chanin9@gmail.com>
2023-09-28 17:18:37 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,13 @@
   <div class="home-qonnect">
     <img alt="Qonnect logo" src="../assets/qonnect_icon.svg" />
     <h2>Please go to Login Page</h2>
+    <h2>Or User Can Input an App Name</h2>
+    <input
+      type="text"
+      v-model="inputValue"
+      @keydown.enter="handleEnter"
+      placeholder="App Name"
+    />
   </div>
 </template>
 
@@ -15,6 +22,22 @@
 //     HelloWorld,
 //   },
 // };
+export default {
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    handleEnter(event) {
+      if (event.keyCode === 13) {
+        if (this.inputValue !== "") {
+          location.href = `/${this.inputValue}`;
+        }
+      }
+    },
+  },
+};
 </script>
 <style>
 .home-qonnect {
